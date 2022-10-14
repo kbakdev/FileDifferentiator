d6008e7597ed30f07e25cf24351fa74972a4694e
Kacper Bąk <kacper.bak@sonderfin.com>
2022-10-14 20:35:37 +0200

    chore: cleanup

--- src/logic/FileType.java
+++
@@ -1,7 +0,0 @@
-package logic;
-
-// FileType is enum which contains all file types with extensions
-public enum FileType {PNG("png"),GIF("gif"),TIFF("tif"),BMP("bmp"), ZIP("zip"), RAR("rar"), PDF("pdf"), MP3("mp3"), AVI("avi"), MKV("mkv"), FLV("flv"), WMV("wmv"), MOV("mov"), XML("xml"), JS("js"), PY("py"), C("c"), CPP("cpp"), H("h"), CS("cs"), JPG("jpg"),;
-    FileType(String extension) {
-    }
-}
